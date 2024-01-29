700ecbf3d2be6d3cf592e153da0203ef54e21d80
Terry Patterson <terrypatterson@gmail.com>
2024-01-29 13:17:35 -0600

    Update data-integration-process-times-set-data-restrictions.sql

--- SaaS/data-integration-process-times-set-data-restrictions.sql
+++ SaaS/data-integration-process-times-set-data-restrictions.sql
@@ -7,9 +7,9 @@ select didss.pk1 as "Data Integration PK1",
 from data_intgr_data_set_status didss
 	join data_intgr di
 	 on didss.data_intgr_pk1 = di.pk1
-where --didss.last_entry_dt < '2021-05-31'
-  --and didss.last_entry_dt > '2021-01-05'
+where --didss.last_entry_dt < '2021-05-31' (In case you want to examine specific dates)
+  --and didss.last_entry_dt > '2021-01-05' (In case you want to examine specific dates)
   --and 
   DATE_PART('hour', last_entry_dt - first_entry_dt) * 60 + DATE_PART('minute', last_entry_dt - first_entry_dt) != '0' and
-  di.name in ('224S Student Integration','223F Student Integration')
+  di.name in ('Integration_Name_Here','')
   order by didss.first_entry_dt desc;
